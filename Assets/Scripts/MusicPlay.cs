5c7fbd17cdd4fce90d6313eeff34f266a2c506a3
NieceyX <65681516+NieceyX@users.noreply.github.com>
2022-10-25 13:58:19 -0600

    tests

--- Assets/Scripts/MusicPlay.cs
+++ Assets/Scripts/MusicPlay.cs
@@ -7,15 +7,23 @@ public class MusicPlay : MonoBehaviour
     [Header("Note  audio")]
     public AudioSource audioSource;
 
-    void Start()
+    void Update()
     {
-
+        Collider[] colls = Physics.OverlapSphere(this.transform.position, 0.7f);
+        foreach (Collider item in colls)
+        {
+            if (item.gameObject.tag == "GameController")
+            {
+                audioSource.Play();
+                Debug.Log("play sound");
+            }
+        }
     }
 
-    void OnCollisionEnter(Collision coll)
+   /* void OnCollisionEnter(Collision coll)
     {
-        //audioSource.Play();
+        audioSource.Play();
         Debug.Log("play sound");
     }
-
+   */
 }
