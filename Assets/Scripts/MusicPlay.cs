7b21f3c28f4ac498dcc0d372a970d322fb89f986
NieceyX <65681516+NieceyX@users.noreply.github.com>
2022-10-27 09:00:00 -0600

    orb fix

--- Assets/Scripts/MusicPlay.cs
+++ Assets/Scripts/MusicPlay.cs
@@ -7,15 +7,37 @@ public class MusicPlay : MonoBehaviour
     [Header("Note  audio")]
     public AudioSource audioSource;
 
+    public int cooldown = 10;
+    private int time = 5;
+
     void Start()
     {
-
+        time = cooldown;
     }
 
-    void OnCollisionEnter(Collision coll)
+    void Update()
     {
-        //audioSource.Play();
-        Debug.Log("play sound");
+        if (time >= cooldown)
+        {
+            Collider[] colls = Physics.OverlapSphere(this.transform.position, 0.1f);
+            foreach (Collider item in colls)
+            {
+                if (item.gameObject.tag == "GameController")
+                {
+                    audioSource.Play();
+                    Debug.Log("play sound");
+                    time = 0;
+                    break;
+                }
+            }
+        }
+        time++;
     }
 
+    /* void OnCollisionEnter(Collision coll)
+     {
+         audioSource.Play();
+         Debug.Log("play sound");
+     }
+    */
 }
