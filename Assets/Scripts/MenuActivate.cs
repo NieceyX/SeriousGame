6a56160d2d4eb20069afbf15b59512df75330096
NieceyX <65681516+NieceyX@users.noreply.github.com>
2022-10-27 11:43:58 -0600

    menu
    exit/restart work, menu opens and closes

---
+++ Assets/Scripts/MenuActivate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActivate : MonoBehaviour
+{
+    public GameObject exit;
+    public GameObject restart;
+
+    public void doActivate()
+    {
+        exit.SetActive(!exit.activeSelf);
+        restart.SetActive(!restart.activeSelf);
+    }
+
+}
