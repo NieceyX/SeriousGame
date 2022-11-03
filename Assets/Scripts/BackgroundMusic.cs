a52500eb764aaad3611dfd4d6e3cfd6be01b9bdd
NieceyX <65681516+NieceyX@users.noreply.github.com>
2022-11-03 12:52:48 -0600

    background music triggers

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Audio;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public AudioEvent music;
+    private float radius = 1f;
+    public LayerMask mask;
+
+    void Update() {
+
+        Collider[] colls = Physics.OverlapBox(this.transform.position, transform.localScale / 2, Quaternion.identity,mask);
+
+        foreach (Collider item in colls)
+            {
+                if (item.gameObject.tag == "GameController")
+                {
+                    AudioManager.PlayEvent(this.music, this.gameObject);
+
+                }
+        }
+    }
+}
