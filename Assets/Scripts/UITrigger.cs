88abb67085a166669a07ee231e5d65ddbe096ead
NieceyX <65681516+NieceyX@users.noreply.github.com>
2022-11-08 11:51:33 -0700

    sounds and UI

---
+++ Assets/Scripts/UITrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITrigger : MonoBehaviour
+{
+    public GameObject menuItem;
+
+    void Start()
+    {
+        menuItem.SetActive(false); 
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Enter");
+        if (col.gameObject.tag == "GameController")
+        {
+            
+            menuItem.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        Debug.Log("Exit");
+        if (col.gameObject.tag == "GameController")
+        {
+            
+            menuItem.SetActive(false);
+        }
+    }
+}
