67749fcab5ffde37776b5f5494d48892cfd8a3a4
junkaichen <junkaichenalex@gmail.com>
2022-11-08 20:17:08 -0700

    Rage Room

--- Assets/Scripts/ThumbstickMover.cs
+++ Assets/Scripts/ThumbstickMover.cs
@@ -14,7 +14,10 @@ public class ThumbstickMover : InputSystemGlobalListener, IMixedRealityInputHand
     private void InputSystemGlobalListenerStart()
     {
         rb = GetComponent<Rigidbody>(); //rb equals the rigidbody on the player
+        transform.position = new Vector3(transform.position.x, 10f, transform.position.z);
     }
+
+    
     public void OnInputChanged(InputEventData<Vector2> eventData)
     {
         if (eventData.MixedRealityInputAction == moveAction)
@@ -24,16 +27,16 @@ public class ThumbstickMover : InputSystemGlobalListener, IMixedRealityInputHand
                 Vector3 temp = new Vector3(eventData.InputData.x, 0f, eventData.InputData.y);
                 Vector3 localDelta = speed * temp;
                 Vector3 tempPosition = transform.position + transform.GetChild(0).rotation * localDelta;
-                /*    transform.position = new Vector3(tempPosition.x, 0.1f, tempPosition.z);*/
+                transform.position = new Vector3(tempPosition.x, 10f, tempPosition.z);
 
-                if (transform.position.x > -10f)
-                {
-                    transform.position = new Vector3(tempPosition.x, 0.1f, tempPosition.z);
-                }
-                else
-                {
-                    transform.position = new Vector3(tempPosition.x + 1, 0.1f, tempPosition.z);
-                }
+                /*                if (transform.position.x > -10f)
+                                {
+                                    transform.position = new Vector3(tempPosition.x, 0.1f, tempPosition.z);
+                                }
+                                else
+                                {
+                                    transform.position = new Vector3(tempPosition.x + 0.1f, 0.1f, tempPosition.z);
+                                }*/
 
             }
         }
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.MixedReality.Toolkit.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    [SerializeField] float PositionXChange = 0.1f;
+    [SerializeField] float PositionZChange = 0.1f;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Debug.Log("acvcc");
+            Debug.Log(other.gameObject.name);
+            other.transform.parent.gameObject.transform.position = new Vector3(other.transform.parent.gameObject.transform.position.x + PositionXChange, 0.1f, other.transform.parent.gameObject.transform.position.z + PositionZChange);
+        }
+        Debug.Log("aaaaa");
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Debug.Log("acvcc");
+            Debug.Log(other.gameObject.name);
+            other.transform.parent.gameObject.transform.position = new Vector3(other.transform.parent.gameObject.transform.position.x + PositionXChange, 0.1f, other.transform.parent.gameObject.transform.position.z + PositionZChange);
+        }
+        Debug.Log("aaaaa");
+    }
+    /*    void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                Debug.Log("acvcc");
+                collision.transform.position = new Vector3(collision.transform.position.x + 0.1f, 0.1f, collision.transform.position.z);
+            }
+            Debug.Log("aaaaa");
+
+
+
+        }*/
+}
